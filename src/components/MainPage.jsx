33a55a7ed1d2c050ff96eea5e56ae7d87c865934
roroism <miroiro0303@gmail.com>
2022-11-13 22:51:21 +0900

    feat: Create page button.

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,7 +2,29 @@ import { useCallback } from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchNewsbySearch, setHistoryToLocalStorage } from "../store/slice";
+import styled from "styled-components";
 import ListItem from "./ListItem";
+
+const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  padding: 0 4%;
+`;
+
+const ButtonDiv = styled.div`
+  padding: 1rem 0;
+  button {
+    &:hover {
+      background-color: #666;
+      color: #fff;
+      border: 1px solid #666;
+    }
+    color: #666;
+    background-color: #fff;
+    border: 1px solid #666;
+    transition: all 0.3s;
+  }
+`;
 
 const MainPage = () => {
   const [searchwords, setSearchWords] = useState();
@@ -40,31 +62,36 @@ const MainPage = () => {
   };
 
   return (
-    <>
+    <Container>
+      <ButtonDiv>
+        <button type="button">clipped News</button>
+      </ButtonDiv>
       <input type="text" onChange={handleChange} />
       {historyList.map((item) => (
         <div key={item}>{item}</div>
       ))}
-      <ul>
-        {newsList.map((item) => (
-          <ListItem
-            key={item._id}
-            id={item._id}
-            main={item.headline.main}
-            date={item.pub_date}
-            section={item.section_name}
-            multimedia={item.multimedia}
-            web_url={item.web_url}
-          />
-          // <div key={item._id} style={{ border: "1px solid black" }}>
-          //   <div>{item.headline.main}</div>
-          //   <div>{item.pub_date.replace("T", " ").substring(0, 19)}</div>
+      <main>
+        <ul>
+          {newsList.map((item) => (
+            <ListItem
+              key={item._id}
+              id={item._id}
+              main={item.headline.main}
+              date={item.pub_date}
+              section={item.section_name}
+              multimedia={item.multimedia}
+              web_url={item.web_url}
+            />
+            // <div key={item._id} style={{ border: "1px solid black" }}>
+            //   <div>{item.headline.main}</div>
+            //   <div>{item.pub_date.replace("T", " ").substring(0, 19)}</div>
 
-          //   <div>id: {item._id}</div>
-          // </div>
-        ))}
-      </ul>
-    </>
+            //   <div>id: {item._id}</div>
+            // </div>
+          ))}
+        </ul>
+      </main>
+    </Container>
   );
 };
 
