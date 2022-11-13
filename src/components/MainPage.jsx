28d79c04c97066d236f5cc7ce905f5c7cc278f61
roroism <miroiro0303@gmail.com>
2022-11-14 02:11:36 +0900

    feat: Create page button.

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -11,21 +11,6 @@ const Container = styled.div`
   padding: 0 4%;
 `;
 
-const ButtonDiv = styled.div`
-  padding: 1rem 0;
-  button {
-    &:hover {
-      background-color: #666;
-      color: #fff;
-      border: 1px solid #666;
-    }
-    color: #666;
-    background-color: #fff;
-    border: 1px solid #666;
-    transition: all 0.3s;
-  }
-`;
-
 const MainPage = () => {
   const [searchwords, setSearchWords] = useState();
   const historyList = useSelector(({ history }) => history.history);
@@ -63,9 +48,6 @@ const MainPage = () => {
 
   return (
     <Container>
-      <ButtonDiv>
-        <button type="button">clipped News</button>
-      </ButtonDiv>
       <input type="text" onChange={handleChange} />
       {historyList.map((item) => (
         <div key={item}>{item}</div>
@@ -82,12 +64,6 @@ const MainPage = () => {
               multimedia={item.multimedia}
               web_url={item.web_url}
             />
-            // <div key={item._id} style={{ border: "1px solid black" }}>
-            //   <div>{item.headline.main}</div>
-            //   <div>{item.pub_date.replace("T", " ").substring(0, 19)}</div>
-
-            //   <div>id: {item._id}</div>
-            // </div>
           ))}
         </ul>
       </main>
